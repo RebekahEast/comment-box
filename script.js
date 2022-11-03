f91b1962e2ab9581e8a5a9bf270d581a879a3f99
RebekahEast <rebekah.jean.east@gmail.com>
2022-11-03 17:13:04 +0000

    some style changes and JS character count

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("comments");
+var commentinput = document.getElementById("comment-input");
+var wordcount = document.getElementById("word-count");
+
+commentinput.addEventListener("keyup",function(){
+    var characters = commentinput.value.split('');
+    wordcount.innerText = characters.length;
+});
