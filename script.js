0935f8b8357886cde96fa297f1fa79cce554380b
RebekahEast <rebekah.jean.east@gmail.com>
2022-11-08 13:56:01 +0000

    submitting changes

--- script.js
+++ script.js
@@ -5,12 +5,19 @@ function countChars(obj){
 
     if (overlength < 0) {
         document.getElementById('charNum').innerHTML = '<span style = "color:red; font-weight:bold;"> You are over the word count of ' + maxlength + ' characters by ' + -overlength + '. </span>';
-        document.getElementById("words").style.backgroundColor = "red";
+        document.getElementById("words").style.borderColor = "red";
     } else {
         document.getElementById('charNum').innerHTML = stdlength + ' out of ' + maxlength + ' characters';
+        document.getElementById("words").style.borderColor = "grey";
         }
 }
 
-comments.addEventListener('submit', (event)=> {
-    event.preventDefault();
+
+
+const commentForm = document.getElementById("comments");
+
+comments.addEventListener("submit", (event) => {
+    event.preventDefault(); //stops the submission and resetting the page
+    console.log("form has been submitted");
 });
+
