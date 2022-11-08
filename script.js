b70587f48262d1b7b0d4f9a1ae95ba66d680301a
RebekahEast <rebekah.jean.east@gmail.com>
2022-11-08 16:00:04 +0000

    used a function to let the form be submitted and shown

--- script.js
+++ script.js
@@ -3,21 +3,31 @@ function countChars(obj){
     var stdlength = obj.value.length;
     var overlength = (maxlength - stdlength);
 
-    if (overlength < 0) {
-        document.getElementById('charNum').innerHTML = '<span style = "color:red; font-weight:bold;"> You are over the word count of ' + maxlength + ' characters by ' + -overlength + '. </span>';
-        document.getElementById("words").style.borderColor = "red";
-    } else {
-        document.getElementById('charNum').innerHTML = stdlength + ' out of ' + maxlength + ' characters';
-        document.getElementById("words").style.borderColor = "grey";
-        }
+if (overlength < 0) {
+    document.getElementById('charNum').innerHTML = '<span style = "color:red; font-weight:bold;"> You are over the word count of ' + maxlength + ' characters by ' + -overlength + '. </span>';
+    document.getElementById("words").style.borderColor = "red";
+} else {
+    document.getElementById('charNum').innerHTML = stdlength + ' out of ' + maxlength + ' characters';
+    document.getElementById("words").style.borderColor = "grey";
+    }
 }
-
-
-
-const commentForm = document.getElementById("comments");
 
 comments.addEventListener("submit", (event) => {
     event.preventDefault(); //stops the submission and resetting the page
     console.log("form has been submitted");
+    var comment_input = document.getElementById('words');
+    var name_input = document.getElementById('name');
+    var email_input = document.getElementById('email');
+    name_input.value = ''; // resets the form back to empty
+    comment_input.value = '';
+    email_input.value = '';
+    // FIX? how to reset the word counter
 });
 
+function myfunction () {
+    let comments = document.querySelector("#words");
+    let submitted_comment = document.querySelector("#submitted_comment");
+
+    submitted_comment.innerHTML = comments.value;
+    
+}
