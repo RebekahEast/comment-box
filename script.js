9fd0a8a334e6b223041a9c4bd11d9ebf8a468281
RebekahEast <rebekah.jean.east@gmail.com>
2022-11-14 18:15:48 +0000

    console loggin
    checking that the inputs appear

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+var form = document.getElementById("comments");
+function submit_form(event) { 
+    event.preventDefault(); 
+    let submitted_comment = document.getElementById("words").value;
+    let submitted_name = document.getElementById("name").value;
+    let submitted_email = document.getElementById("email").value;
+    console.log(submitted_comment);
+    console.log(submitted_email);
+    console.log(submitted_name);
+} 
+form.addEventListener('submit', submit_form);
+
 function countChars(obj){
     var maxlength = 140;
     var stdlength = obj.value.length;
@@ -6,16 +18,10 @@ function countChars(obj){
         document.getElementById('charNum').innerHTML = '<span style = "color:red; font-weight:bold;"> You are over the word count of ' + maxlength + ' characters by ' + -overlength + '. </span>';
         document.getElementById("send").disabled = true;
         document.getElementById("words").style.borderColor = "red";
-    } else {
+    }
+    if (maxlength > stdlength)  {
         document.getElementById('charNum').innerHTML = stdlength + ' out of ' + maxlength + ' characters';
+        document.getElementById("words").disabled = false;
         document.getElementById("words").style.borderColor = "grey";
     }
-}
-
-function submitted (event) {
-    event.preventDefault ();
-    let submitted_comment = document.getElementById("words");
-    let submitted_name = document.getElementById("name");
-    let submitted_email = document.getElementById("email");
-
 }
